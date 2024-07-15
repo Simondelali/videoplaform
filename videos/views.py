137c502065b537dcbbc16542516ef878a9d630b1
Simondelali <atiegardelali@gmail.com>
2024-07-15 21:45:14 +0000

    Add video_page view

--- videos/views.py
+++ videos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import Video
 
-# Create your views here.
+@login_required
+def video_page(request, video_id):
+    video = get_object_or_404(Video, id=video_id)
+    next_video = Video.objects.filter(id__gt=video_id).order_by('id').first()
+    previous_video = Video.objects.filter(id__lt=video_id).order_by('-id').first()
+
+    context = {
+        'video': video,
+        'next_video': next_video,
+        'previous_video': previous_video,
+    }
+    return render(request, 'videos/video_page.html', context)
