bb3bd6d037d86a0dc23e49986885662f543475a4
Simondelali <atiegardelali@gmail.com>
2024-07-16 01:06:26 +0000

    Fix CustomUser() got unexpected keyword arguments: 'is_verified'

--- users/models.py
+++ users/models.py
@@ -16,7 +16,6 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         extra_fields.setdefault('is_active', True)
-        extra_fields.setdefault('is_verified', True)
         return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
