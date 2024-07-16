82518c67a794945422a4b2d18c32df08fd842292
Simondelali <atiegardelali@gmail.com>
2024-07-16 00:23:27 +0000

    Fix login redirect for @login_required decorator

--- core/settings.py
+++ core/settings.py
@@ -135,6 +135,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGIN_URL = '/users/login/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
