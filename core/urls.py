0e4f46c221ad180f061a13d13cb32282f8433d35
Simondelali <atiegardelali@gmail.com>
2024-07-16 00:27:47 +0000

    Update video_page url pattern

--- core/urls.py
+++ core/urls.py
@@ -18,9 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from users.views import home
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('', home, name='home'),
-    path('videos/', include('videos.urls')),
+    path('video/', include('videos.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
