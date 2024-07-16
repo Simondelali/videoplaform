bd7f9ff9083c9164d477cac1f759d642338854a1
Simondelali <atiegardelali@gmail.com>
2024-07-16 18:44:27 +0000

    fix: Update db engine

--- core/settings.py
+++ core/settings.py
@@ -98,6 +98,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #     }
 # }
 
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = {pair.split('=')[0]:pair.split('=')[1] for pair in connection_string.split(' ')}
+
+DATABASES = {
+
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME' : parameters['dbname'],
+        'HOST': parameters['host'],
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
+        'PORT': parameters['port'],
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
