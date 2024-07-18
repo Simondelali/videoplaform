3d359edc2baad541b12836778fe36940e4473efe
Simondelali <atiegardelali@gmail.com>
2024-07-18 02:19:26 +0000

    feature: Add comments section

--- videos/admin.py
+++ videos/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Video
+from .models import Video, Comment
 
 @admin.register(Video)
 class VideoAdmin(admin.ModelAdmin):
     list_display = ('title', 'uploaded_at')
     search_fields = ('title', 'description')
+
+admin.site.register(Comment)
--- videos/urls.py
+++ videos/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.default_video_page, name='default_video_page'),
     path('<slug:slug>/', views.video_page, name='video_page'),
     path('increment-view/<slug:slug>/', views.increment_view, name='increment_view'),
+
+    path('<slug:slug>/comment/', views.post_comment, name='post_comment'),
+    path('<slug:slug>/comment/<int:comment_id>/reply/', views.post_reply, name='post_reply'),
+    path('<slug:slug>/comment/<int:comment_id>/delete/', views.delete_comment, name='delete_comment'),
 ]
