4482a11d023fa135bbdb10626a99909e30726aad
Simondelali <atiegardelali@gmail.com>
2024-07-19 02:24:24 +0000

    Set Courier Auth token

--- core/deployment.py
+++ core/deployment.py
@@ -3,7 +3,7 @@ from .settings import *
 from .settings import BASE_DIR
 
 SECRET_KEY = [os.environ['SECRET_KEY']]
-auth_token = [os.environ['auth_token']]
+auth_token = [os.environ['COURIER_AUTH_TOKEN']]
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
 CSRF_TRUSTED_ORIGINS = ['https://'+ os.environ['WEBSITE_HOSTNAME']]
 DEBUG = True
